5365fd138abf0a90076fe39d03c8cc86c702aca2
Gord Letkeman <rndedg@gmail.com>
2022-07-12 21:01:36 +0000

    Added assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`👍️👍️👍️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎️👎️👎️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
