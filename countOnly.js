1cdcea3807769b13c85bb47443632e3a6e271097
Gord Letkeman <rndedg@gmail.com>
2022-07-18 17:27:27 +0000

    added countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`👍️👍️👍️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎️👎️👎️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+       results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+}
