2e438e0af9fcd61e1fffdce59f6a058823e68f8a
Gord Letkeman <rndedg@gmail.com>
2022-08-02 20:43:58 +0000

    added Mocha/Chai testing

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,17 +1,3 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`👍️👍️👍️ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`👎️👎️👎️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-
-
-
-
-
 // Implement the function findKeyByValue which takes in an object and a value. It should scan the object and return the first key which contains the given value. If no key with that given value is found, then it should return undefined.
 
 
@@ -27,3 +13,4 @@ const findKeyByValue = function(object, value) {
   return undefined;
 };
 
+module.exports = findKeyByValue;
