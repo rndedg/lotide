e13ef1fab4a2b3d1004520dc7bc14e1d97f12d3a
Gord Letkeman <rndedg@gmail.com>
2022-08-02 19:13:20 +0000

    added Mocha/Chai testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,12 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] when passed ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ['Lighthouse', 'Labs']);
+  });
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+  it("returns [2, 3, 4, 5] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]); 
+  });
+});
