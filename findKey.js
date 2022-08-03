f6bdb1ab0dfd2e6ab90ca3f967684f193a8f220e
Gord Letkeman <rndedg@gmail.com>
2022-08-03 03:08:22 +0000

    added Mocha/Chai testing

--- findKey.js
+++ findKey.js
@@ -1,15 +1,3 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`👍️👍️👍️ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`👎️👎️👎️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-
-
-
 const findKey = function(object, callback) {
   for (const key in object) {
     if (callback(object[key])) {
@@ -19,20 +7,4 @@ const findKey = function(object, callback) {
   } return undefined;
 };
 
-
-
-console.log(assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), "noma"));
-
-console.log(assertEqual(findKey({
-  "Pepperoni":   { slices: 5 },
-  "Veggie":      { slices: 12 },
-  "Meat Lovers": { slices: 5 },
-  "Cheese":      { slices: 5 }
-}, x => x.slices === 12), "Veggie"));
+module.exports = findKey;
